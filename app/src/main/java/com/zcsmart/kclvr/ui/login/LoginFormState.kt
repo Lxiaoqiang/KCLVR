6512dceee125debe8df2ddcffddabc56b11e4d43
lihuiqiang <15116393675@163.com>
2019-06-25 19:19:42 +0800

    init commit

---
+++ app/src/main/java/com/zcsmart/kclvr/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.zcsmart.kclvr.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
